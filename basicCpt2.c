e49be311097df1125106e54c23a7e99067171548
bellamxrgarita <idayrit26@gwmail.gwu.edu>
2024-08-28 14:37:46 -0400

    practice

---
+++ basicCpt2.c
@@ -0,0 +1,13 @@
+// the standard c library with majority of functions needed
+# include <stdlib.h>
+// the input output lib
+# include <stdio.h>
+
+// to compile, you do gcc nameoffile.c
+// to run, you do ./a.out
+
+int main(void){
+    printf("Hello World");
+    // this symbolizes the end of the program
+    return 0;
+}
